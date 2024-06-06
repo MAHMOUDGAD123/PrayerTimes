25c24583d11a38f2161b092ae70fc539e086a69a
Mahmoud_Gad <hooda.gad333@gmail.com>
2024-06-06 21:38:29 +0300

    make a small change in ( time.js & api.js ) files

--- js/time.mjs
+++ js/time.mjs
@@ -4,11 +4,12 @@ export const frmt_time = (n) => {
   return Math.floor(Math.log10(n)) ? n : "0" + n;
 };
 
-const hours = document.querySelector(".clock > .time > .hr");
-const minutes = document.querySelector(".clock > .time > .min");
-const seconds = document.querySelector(".clock > .time > .sec");
 
 setInterval(() => {
+  const hours = document.querySelector(".clock > .time > .hr");
+  const minutes = document.querySelector(".clock > .time > .min");
+  const seconds = document.querySelector(".clock > .time > .sec");
+  
   const D = new Date();
   const hrs = frmt_time(D.getHours());
   const mins = frmt_time(D.getMinutes());
