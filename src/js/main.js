c53011cbdd0935757fd58f44fcd34666b0827699
Mahmoud_Gad <hooda.gad333@gmail.com>
2025-10-11 21:52:25 +0300

    fukin fix the shit

--- src/js/main.js
+++ src/js/main.js
@@ -37,20 +37,17 @@ let isEnglish = read_lang();
 window.addEventListener(
   "DOMContentLoaded",
   () => {
-    console.log("lang fix");
     /** @type {HTMLElement} */
     const loadingEle = document.querySelector(".loading");
     /** @type {boolean} */
     const isEn = _Storage.read("__prayertimes_lang__", "localStorage");
-    if (!isEn) {
-      loadingEle.style.direction = "rtl";
-      /**@type {HTMLElement[]} */
-      const all_txt = loadingEle.querySelectorAll("[data-en]");
-      all_txt.forEach((el) => {
-        const en_txt = el.dataset.en;
-        el.textContent = en_ar.get(en_txt);
-      });
-    }
+    if (isEn) return;
+    /**@type {HTMLElement[]} */
+    const all_txt = loadingEle.querySelectorAll("[data-en]");
+    all_txt.forEach((el) => {
+      const en_txt = el.dataset.en;
+      el.textContent = en_ar.get(en_txt);
+    });
   },
   { once: true }
 );
